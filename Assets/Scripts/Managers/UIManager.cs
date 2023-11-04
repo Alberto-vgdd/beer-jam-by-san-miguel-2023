56cfb0f2a2ad9e03a8f6d297e8b819db0e99c70e
Alberto Gómez <alberto.vgdd@gmail.com>
2023-11-04 12:35:50 +0100

    Bug fixes

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -23,8 +23,8 @@ public class UIManager : MonoBehaviour
     private GameOverScreen soloGameOverScreen;
     private GameOverScreen multiGameOverScreen;
 
-    // [SerializeField]
-    // private GameFinishedDisplay[] gameFinishedDisplays;
+    [SerializeField]
+    private GameFinishedDisplay gameFinishedDisplay;
 
     void Awake()
     {
@@ -73,7 +73,10 @@ public class UIManager : MonoBehaviour
         soloGameplayScreenGameObject.SetActive(InputManager.NUMBER_OF_PLAYERS == 1);
         multiplayerGameplayScreenGameObject.SetActive(InputManager.NUMBER_OF_PLAYERS == 2);
 
-        // gameFinishedDisplays[InputManager.NUMBER_OF_PLAYERS - 1].Reset();
+        if (InputManager.NUMBER_OF_PLAYERS == 2)
+        {
+            gameFinishedDisplay.Reset();
+        }
 
     }
 
