a838f8bb855b763c977bc2c1e0fe98437b5e37d9
Alberto Gómez <alberto.vgdd@gmail.com>
2023-11-22 22:30:43 +0100

    change chef font color.

--- Assets/Scripts/User Interface/ChefAnimationController.cs
+++ Assets/Scripts/User Interface/ChefAnimationController.cs
@@ -23,7 +23,7 @@ public class ChefAnimationController : MonoBehaviour
     }
     private void OnEnable()
     {
-       DOTween.Sequence().Append(chefSprite.DOMoveY(showPos.transform.position.y, 1)).Append(textBackGround.DOFade(1, 1)).Append(chefTextField.DOText(chefText, 1.5f)).OnComplete(KillChef);
+        DOTween.Sequence().Append(chefSprite.DOMoveY(showPos.transform.position.y, 1)).Append(textBackGround.DOFade(1, 1)).Append(chefTextField.DOText(chefText, 1.5f)).OnComplete(KillChef);
     }
 
     private void OnDisable()
@@ -33,9 +33,9 @@ public class ChefAnimationController : MonoBehaviour
         textBackGround.alpha = 0;
     }
 
-    void KillChef() 
+    void KillChef()
     {
         DOTween.Sequence().PrependInterval(60).Append(textBackGround.DOFade(0, 0.5f)).Append(chefSprite.DOMoveY(initialPos.transform.position.y, 1f)).Append(chefTextField.DOText("", 0));
-        
+
     }
 }
